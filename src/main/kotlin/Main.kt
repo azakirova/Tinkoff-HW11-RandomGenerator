55722e1814fdf845349c237dc176bbb1db71ef44
azakirova <akov93@gmail.com>
2022-05-06 22:42:54 +0700

    Add RandomGenerator.jar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
-fun main(){
+fun main(args: Array<String>) {
     print("Введите целое число от 1 до 30: ")
     val n = readln().toIntOrNull()
 
-    if (n!=null && n>=1 && n<=30){
+    if (n != null && n >= 1 && n <= 30) {
         val generator = Generator()
         val listPerson = generator.generate(n)
         val pdfManager = PDFManager()
         pdfManager.fillPdfFile(listPerson)
 
-    }
-    else print("Некорректный ввод")
+    } else print("Некорректный ввод")
 }
